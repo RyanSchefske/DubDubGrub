f688e0989a9d5fde3f68552d8b30865db29f5c25
Ryan Schefske <ryanschefske@gmail.com>
2021-07-13 19:14:18 -0500

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Ryan Schefske on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppTabView()
+        }
+    }
+}
---
+++ DubDubGrub/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  DubDubGrub
+//
+//  Created by Ryan Schefske on 7/12/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+		Text("Profile")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
